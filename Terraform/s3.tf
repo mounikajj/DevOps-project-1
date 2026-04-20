59af51e081d88fe8e1359e4830dc89b1cd116077
Mounika <jmounika1015@gmail.com>
2026-04-20 18:15:58 +0530

    Update s3.tf

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,1 +1,11 @@
+resource "aws_s3_bucket" "s3_buckets" {
+
+  for_each = toset([
+    "for-each-demo-bucket-terraform",
+    "for-each-demo-bucket-terraform",
+    "for-each-demo-bucket-terraform"
+  ])
+
+  bucket = each.value
+}
 
