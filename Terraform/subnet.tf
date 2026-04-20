b4e56f14d15e8a7720b1ff698dd0fb2c02b94099
Mounika <jmounika1015@gmail.com>
2026-04-20 18:16:19 +0530

    Update subnet.tf

--- Terraform/subnet.tf
+++ Terraform/subnet.tf
@@ -1,1 +1,16 @@
+resource "aws_subnet" "this" {
+
+  for_each = {
+    s3-bucket-tf-az-1  = "10.0.1.0/24"
+    s3-bucket-tf-az-2 = "10.0.2.0/24"
+    s3-bucket-tf-az-3 = "10.0.3.0/24"
+  }
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value
+
+  tags = {
+    Name = each.key
+  }
+}
 
