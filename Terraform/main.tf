8af89160c34931b8a4b699291841e025da153028
Mounika <jmounika1015@gmail.com>
2026-04-20 18:15:35 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,21 @@
+# 1. Version Settings
+terraform {
+  required_version = ">= 1.1.0" # Minimum CLI version allowed
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Use any version from 5.0 to 5.9
+    }
+  }
+}
+
+# 2. Provider Configuration
+provider "aws" {
+  region = "ap-south-1" # Mumbai Region
+}
+
+
+
+
 
